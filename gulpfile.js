9f8dcadc03e898b6e87b7dc722b71f146df3a22a
Pavel K <pa.kachalov@gmail.com>
2018-03-03 01:24:20 +0300

    gulp task for style

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,21 @@
 "use strict";
 
 var gulp = require("gulp");
+var sass = require("gulp-sass");
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+
+gulp.task("style", function() {
+  gulp
+    .src("source/sass/style.scss")
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(postcss([autoprefixer()]))
+    .pipe(gulp.dest("build/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"));
+});
